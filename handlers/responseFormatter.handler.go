77d1b7df1176717d83086f175d2661b321d8ed64
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2024-06-20 09:35:35 +0700

    ♻️ Refactor Service Response Error Became Interface

--- handlers/responseFormatter.handler.go
+++ handlers/responseFormatter.handler.go
@@ -18,7 +18,7 @@ type ServiceResponse struct {
 	Status  int         `json:"status"`
 	Message string      `json:"message"`
 	Data    interface{} `json:"data"`
-	Err     error       `json:"error"`
+	Err     interface{} `json:"error"`
 }
 
 func ResponseFormatter(c *gin.Context, status int, data interface{}, message string) {
