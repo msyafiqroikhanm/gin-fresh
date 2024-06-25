71efd910be16e7b318d6e3f948e37a24461944f8
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2024-06-25 10:55:10 +0700

    ♻️ Refactor usr_feature and usr_role To Have Many To Many Relationship WIth usr_rolefeautre As Join Table

--- models/usr_role.model.go
+++ models/usr_role.model.go
@@ -3,9 +3,10 @@ package models
 import "gorm.io/gorm"
 
 type USR_Role struct {
-	ID               uint   `gorm:"primaryKey" json:"id"`
-	Name             string `json:"name" validate:"required"`
-	IsAdministrative bool   `json:"is_administrative"`
+	ID               uint           `gorm:"primaryKey" json:"id"`
+	Name             string         `json:"name" validate:"required"`
+	IsAdministrative bool           `json:"is_administrative"`
+	Features         []*USR_Feature `gorm:"many2many:usr_rolefeatures;" json:"features"`
 	gorm.Model
 }
 
